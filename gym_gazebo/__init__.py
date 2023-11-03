38362aa4bae178878c537709b848aca0ed7818ff
paigejh13 <piage.hall1313@gmail.com>
2023-11-03 15:51:09 -0700

    lab 8

---
+++ gym_gazebo/__init__.py
@@ -0,0 +1,20 @@
+import logging
+from gym.envs.registration import register
+
+logger = logging.getLogger(__name__)
+
+# Gazebo
+# ----------------------------------------
+
+# cart pole
+register(
+    id='GazeboCartPole-v0',
+    entry_point='gym_gazebo.envs.gazebo_cartpole:GazeboCartPolev0Env',
+)
+
+#line follow
+register(
+    id='Gazebo_linefollow-v0',
+    entry_point='gym_gazebo.envs.gazebo_linefollow:Gazebo_Linefollow_Env',
+    max_episode_steps=3000,
+)
